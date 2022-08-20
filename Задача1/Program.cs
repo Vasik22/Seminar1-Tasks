8f8d71c193b0b77bdeef1bf72078af1c96b039a1
Your Name <you@example.com>
2022-08-20 21:53:40 +0300

    Task1

---
+++ Задача1/Program.cs
@@ -0,0 +1,13 @@
+﻿//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+Console.WriteLine("Enter number1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number2");
+int number2=Convert.ToInt32(Console.ReadLine());
+if (number1 >=number2){
+Console.WriteLine("MaxNumber "+number1);
+}
+else {
+   Console.WriteLine("MaxNumber"+number2); 
+}
