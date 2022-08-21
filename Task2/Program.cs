1ef2bb990791274bc8430680d185da59298f8b79
Your Name <you@example.com>
2022-08-21 11:17:05 +0300

    Rename

---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Enter number1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number3");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 >=number2){
+Console.WriteLine ("MaX"+ number1);
+}
+
+else {
+Console.WriteLine ("MaX"+ number2);
+}
+if (number2 > number3){
+Console.WriteLine ("MaX"+ number2);
+}
+else {
+   Console.WriteLine ("MaX"+ number3);
+}
+if (number1 > number3) {
+Console.WriteLine ("MaX"+ number1);
+}
+else {
+    Console.WriteLine ("MaX"+ number3);
+}
