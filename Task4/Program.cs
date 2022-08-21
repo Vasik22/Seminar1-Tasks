b5eca27747c62d1c6c109a394792d83d4b08c529
Your Name <you@example.com>
2022-08-21 11:30:03 +0300

    task4

---
+++ Task4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter number:");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+for (i = 1; i<number; i++){
+    if  (i % 2==0) {
+        Console.WriteLine(i);
+    }
+}
