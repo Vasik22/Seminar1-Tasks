7d26f76403964ce224cf765a1e30fb7d61fcbd05
Your Name <you@example.com>
2022-08-21 11:24:08 +0300

    task3

---
+++ Task3/Program.cs
@@ -0,0 +1,9 @@
+﻿ Console.Write("Enter number:");
+int number = Convert.ToInt32(Console.ReadLine());
+ if  (number % 2==0)
+{
+ Console.Write("четное число");  
+ }
+ else {
+    Console.Write("нечетное число");
+ }
