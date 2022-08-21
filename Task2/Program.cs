59d915b6857039b8f1e0f8b9b236523179d8e7dc
Your Name <you@example.com>
2022-08-21 11:20:19 +0300

    Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,26 +1,18 @@
-﻿Console.WriteLine("Enter number1");
-int number1 = Convert.ToInt32(Console.ReadLine());
+﻿Console.WriteLine("Enter number");
+int number = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter number2");
 int number2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter number3");
 int number3 = Convert.ToInt32(Console.ReadLine());
 
-if (number1 >=number2){
-Console.WriteLine ("MaX"+ number1);
-}
-
-else {
-Console.WriteLine ("MaX"+ number2);
-}
-if (number2 > number3){
-Console.WriteLine ("MaX"+ number2);
-}
-else {
-   Console.WriteLine ("MaX"+ number3);
-}
-if (number1 > number3) {
-Console.WriteLine ("MaX"+ number1);
-}
-else {
-    Console.WriteLine ("MaX"+ number3);
+if (number > number2)
+ {
+    number2 = number;
+ }
+if (number3 > number2)
+{
+    Console.WriteLine ( " Max "+number3);
+ } else
+{
+  Console.WriteLine ( " Max "+number2);   
 }
